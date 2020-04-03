37123c7eca8c0566185c5865f2bf0acfcfbf00ef
Mike <m_kanyar@encs.concordia.ca>
2020-04-03 00:53:07 -0400

    adding the clicks to all the buttons

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-alert("hello");
+var numberOfKeys = document.querySelectorAll(".drum").length;
+for (var i = 0; i < numberOfKeys; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    alert("I got Clicked");
+  });
+}
